22eab26af0a1ab98ea698967d6b500ff790e9c7a
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-03-29 11:38:55 +0100

    Updated

--- image_generation_AI.py
+++ image_generation_AI.py
@@ -46,3 +46,4 @@ if st.button('Generate Image'):
         with open(filename, 'rb') as f:
             image_data = f.read()
         download = st.download_button(label="Download Image", data=image_data, file_name="image_generated.jpg")
+        
